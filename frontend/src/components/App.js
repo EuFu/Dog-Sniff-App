ff4c1ad0c5b59289e08308f7b1f2f2a2ed3fb396
EuFu <88345318+EuFu@users.noreply.github.com>
2022-05-02 17:27:09 -0700

    change

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -39,7 +39,7 @@ function App() {
 
   useEffect(() => {
     async function getDog() {
-      await axios.get("/api").then((data) => {
+      await axios.get("/").then((data) => {
         setAllDogs(data.data);
         // console.log(data.data);
       });
